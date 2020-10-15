6a1d182bc0aeaa3c483b9fc7ebab6d76802b1618
henhen724 <hshunt@uchicago.edu>
2020-10-15 04:06:11 -0400

    Getting Start button fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,8 +71,8 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/intro')}>
-              Get Started
+              to={useBaseUrl('docs/howtocontrib')}>
+              Getting Started
             </Link>
           </div>
         </div>
