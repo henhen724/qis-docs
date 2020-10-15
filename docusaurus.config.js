5539100a284a417c64ee5e846bcb1d1397e1a9db
henhen724 <hshunt@uchicago.edu>
2020-10-15 02:55:29 -0400

    editUrl fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -68,9 +68,8 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/henhen724/qis-docs/edit/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
